7d55fc17100b3661f9da20b59f45a8c04b364037
Alexander.German <questlogilt@mail.ru>
2020-06-14 21:05:58 +0300

    lesson05

--- script.js
+++ script.js
@@ -1,35 +1,21 @@
-let lang = 'ru';
+let arr = ['236', '698', '547', '14', '496', '256', '245'];
+const frst = ['2', '4'];
 
-    if (lang == 'ru') {
-	    var arr = ['пн', 'вт', 'ср', 'чт', 'пт', 'сб', 'вс'];
-  }
-  
-    if (lang == 'en') {
-	    arr = ['mn', 'ts', 'wd', 'th', 'fr', 'st', 'sn'];
-  }
-
-    alert(arr);
-
-
-
-let switchLang = 'ru';
-  switch (switchLang){
-    case 'ru':
-      alert('пн, вт, ср, чт, пт, сб, вс');
-    break;
-    case 'en':
-      alert('mn, ts, wd, th, fr, st, sn');
-    break;
+for (let v of arr) {
+ if (frst.includes(String(v).charAt(0)))
+  console.log(v);
 }
 
-let lang03 = 'en';
-let arr03 = {'ru':['пн','вт','ср','чт','пт','сб','вс'],
-'en':['mo','tu','we','th','fr','sa','su']};
-
-alert(arr03[lang03]);
 
 
-let namePerson = prompt("Введите ваше имя");
-namePerson === "Артем" ? console.log("директор") :
-namePerson === "Максим" ? console.log("преподаватель") : 
-console.log("cтудент");
+let n = 100;
+
+next:
+for (let i = 2; i <= n; i++) { 
+
+  for (let j = 2; j < i; j++) { 
+    if (i % j == 0) continue next; 
+  }
+
+  console.log('Число:' + i + ' Делители этого числа: 1 и ' + i);
+};
